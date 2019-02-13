e5bf691d98fe7712dd9c8b7142895a3850c055da
Vadim Bakaev <bakaev.vad@gmail.com>
2019-02-13 22:27:15 +0100

    Add Solutions

---
+++ Solutions.hs
@@ -0,0 +1,23 @@
+module Solutions where
+
+myLast :: [a] -> a
+myLast (x : []) = x
+myLast (x : xs) = myLast xs
+
+myButLast :: [a] -> a
+myButLast (x : _ : []) = x
+myButLast (x : xs)     = myButLast xs
+
+elementAt :: [a] -> Int -> a
+elementAt (x:_) 1  = x
+elementAt (_:xs) n = elementAt xs (n - 1)
+
+myLength :: [a] -> Int
+myLength = loop 0
+           where loop n []     = n
+                 loop n (x:xs) = loop (n + 1) xs
+
+myReverse :: [a] -> [a]
+myReverse xs = reverseInto [] xs
+               where reverseInto acc []     = acc
+                     reverseInto acc (x:xs) = reverseInto (x : acc) xs
