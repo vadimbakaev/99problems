b4a317416f6dafe4277ab779d03cf758c7914827
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-04 20:31:15 +0100

    Fix range

--- Solutions03.hs
+++ Solutions03.hs
@@ -6,5 +6,6 @@ insertAt x xs i = left ++ x:right
     where (left, right) = splitAt (i - 1) xs
 
 range :: Int -> Int -> [Int]
-range m n | m > n     = []
-          | otherwise = m : (range (m + 1) n)
+range m n | m == n     = [n]
+          | m < n      = m : (range (m + 1) n)
+          | otherwise  = m : (range (m - 1) n)
