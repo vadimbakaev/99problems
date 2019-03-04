47f21a3f782bcca1b10cc58212c969477224f34f
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-04 20:27:47 +0100

    Add range

--- Solutions03.hs
+++ Solutions03.hs
@@ -4,3 +4,7 @@ import Data.List
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs i = left ++ x:right
     where (left, right) = splitAt (i - 1) xs
+
+range :: Int -> Int -> [Int]
+range m n | m > n     = []
+          | otherwise = m : (range (m + 1) n)
