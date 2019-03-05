90a6c138c89cfee4bd927d14f130290d8c08eabf
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-05 20:58:36 +0100

    WIP on rnd_select

--- Solutions03.hs
+++ Solutions03.hs
@@ -1,5 +1,6 @@
 module Solutions03 where
 import Data.List
+import System.Random
 
 insertAt :: a -> [a] -> Int -> [a]
 insertAt x xs i = left ++ x:right
@@ -9,3 +10,7 @@ range :: Int -> Int -> [Int]
 range m n | m == n     = [n]
           | m < n      = m : (range (m + 1) n)
           | otherwise  = m : (range (m - 1) n)
+
+rnd_select :: [a] -> Int -> IO [Int]
+rnd_select xs n = do r <- randomIO :: IO Int
+                     return [r `mod` length xs]
