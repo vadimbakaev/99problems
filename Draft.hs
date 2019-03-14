9f3f148816c1dcd3d8b20397221d4b1fc54636ba
Vadim Bakaev <bakaev.vad@gmail.com>
2019-03-14 21:51:16 +0100

    Update fibonacci

--- Draft.hs
+++ Draft.hs
@@ -1,8 +1,6 @@
 module Draft where
 
 fibonacci :: Integer -> Integer -> Integer -> Integer
-fibonacci _ _ 0 = 0
-fibonacci _ _ 1 = 1
-fibonacci _ _ 2 = 1
-fibonacci x y n | n == 3    = x + y
-                | otherwise = fibonacci y (y+x) (n-1) 
+fibonacci x _ 0 = x
+fibonacci _ y 1 = y
+fibonacci x y n = fibonacci y (x + y) (n - 1)
