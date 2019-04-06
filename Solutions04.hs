3242e1d8fdaadc6e2639a77b7fb3ff2f51bfd2f1
Vadim Bakaev <bakaev.vad@gmail.com>
2019-04-06 13:52:16 +0200

    Add myGCD

--- Solutions04.hs
+++ Solutions04.hs
@@ -3,3 +3,7 @@ module Solutions04 where
 isPrime :: Integer -> Bool
 isPrime n = n > 1 && null [x | x <- [2..sqrtN n], n `mod` x == 0]
     where sqrtN = floor . sqrt . fromInteger
+
+myGCD :: Int -> Int -> Int
+myGCD a 0 = abs a
+myGCD a b = myGCD b (a `mod` b) 
